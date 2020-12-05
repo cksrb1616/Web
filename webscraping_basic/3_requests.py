647c700918cd69d4d2a2c7b70de68cb9363c9071
cksrb1616 <53897355+cksrb1616@users.noreply.github.com>
2020-12-05 20:08:39 +0100

    changes

--- webscraping_basic/3_requests.py
+++ webscraping_basic/3_requests.py
@@ -1,2 +1,18 @@
+# At terminal pip install requests
 import requests
-requests.get("http://naver.com")
+res = requests.get("http://google.com")
+print("response code :", res.status_code)
+res.raise_for_status()
+# when there is problem show the problem and end the program
+# 한 문장으로 밑에 if 문장 다 대
+
+#if res.status_code == requests.codes.ok:
+#    print("Normal")
+#else:
+#    print("There is a problem. [error code ", res.status_code, "]")체
+
+print(len(res.text))
+print(res.text)
+
+with open("mygoogle.html", "w", encoding="utf8") as f:
+    f.write(res.text)
